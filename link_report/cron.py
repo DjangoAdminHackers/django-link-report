3bf112fcd3f42716bcf7dad0561280f52fc6a31f
Andy Baker <andy@andybak.net>
2017-02-14 15:55:17 +0000

    add django-cron suppo

---
+++ link_report/cron.py
@@ -0,0 +1,19 @@
+# This file works with our fork of django-cron.
+# It's use is optional
+# Use any means you like to run scheduled jobs.
+
+
+from django_cron import cronScheduler
+from django_cron import Job
+from django_cron import DAY
+from link_report.utils import update_sentry_404s
+
+
+class RunUpdateSentry404s(Job):
+
+        run_every = DAY
+
+        def job(self):
+            update_sentry_404s()
+
+cronScheduler.register(RunUpdateSentry404s)
