47a60aec4829ec14ed7f2771236db924e888d7b8
Chen Zhe <fruitschen@gmail.com>
2019-03-15 12:27:38 +0800

    move ignore urls to a separate file

--- link_report/utils.py
+++ link_report/utils.py
@@ -8,54 +8,7 @@ from urllib import unquote_plus
 from django.utils.dateparse import parse_datetime
 from .models import Sentry404Event, Sentry404Issue, IgnoredUrl
 
-IGNORE_URLS = [
-    
-    "*link.attr(*",
-    '*/tel:*',
-    
-    '*.php*',
-    '*.asp*',
-    '*.htm*',
-    '*.jsp*',
-
-    '*/editor/editor/',
-    '*/ckeditor/editor/',
-    '*/fckeditor/editor/',
-    '*/fck/editor/',
-    '*/Fckeditornew/editor/',
-    '*/Fckeditorold/editor/',
-    '*/FCKeditor/editor/',
-    '*/Fckeditornew/editor/',
-    '*/Fckeditorold/editor/',
-
-    '*/tiny_mce.js/',
-    '*/uploadify.swf/',
-
-    '*/InstallWizard.aspx/*',
-    '*/phpmyadmin/*',
-
-    '/&nx=CLICK_X&ny=CLICK_Y*',
-    '/.git/',
-    '/a.attr/',
-    '/android/',
-    '/administrator/',
-    '/apple-touch-icon-*.png/',
-    '/components/com_content/metadata.xml/',
-    '/feed/',
-    '/ios/',
-    '/js/mage/cookies.js/'
-    '/mobile-app/',
-    '/mysql/',
-    '/rss/',
-    '/sql/',
-    '/typo3/',
-    '/user/register/',
-    '/wordpress/',
-    '/wp/',
-    '/wp-admin/',
-    '/wp-content/*',
-    '/wp-includes/*',
-]
+from .ignore_urls import IGNORE_URLS
 
 ACCEPT_USER_AGENTS = [
     'Amazon Silk',
