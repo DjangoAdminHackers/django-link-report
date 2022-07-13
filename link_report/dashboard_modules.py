140dab4dc262f137551b9e0d145f7e62cae2d412
Chen Zhe <fruitschen@gmail.com>
2022-07-13 20:16:58 +0800

    upgrade to django4

--- link_report/dashboard_modules.py
+++ link_report/dashboard_modules.py
@@ -1,5 +1,5 @@
 from admin_tools.dashboard.modules import DashboardModule
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class Sentry404Errors(DashboardModule):
